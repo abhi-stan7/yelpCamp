014182c934a46dd99cf7c209a2b8e5c6b66d0ecb
abhishek ranjan <abhishekranjan622@gmail.com>
2018-03-29 05:45:54 +0000

    app.js crash

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, 3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Movie app has started");
 });
 

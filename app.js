84388ab829cc2b38d959fa2a49eb3717cccb10c7
abhishek ranjan <abhishekranjan622@gmail.com>
2018-03-29 05:34:56 +0000

    change app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, 3000, function(){
     console.log("Movie app has started");
 });
 
